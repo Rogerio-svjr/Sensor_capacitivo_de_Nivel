71a0fc6c19603053db18db54c2d8a7ab0d50bce4
Rogerio-svjr <rogeriosvjr@gmail.com>
2023-06-13 12:17:16 -0300

    Criação do clock
    Criação do clock que vai alterar o estado do pino 7 do arduino, liga ao trigger do 555

--- Sensor_nivel/Sensor_nivel.ino
+++ Sensor_nivel/Sensor_nivel.ino
@@ -1,7 +1,20 @@
+// Variáveis de tempo
+unsigned long clockTrigger = 0;
+// Variáveis de estado
+bool clockState = LOW; 
+
 void setup() {
+  Serial.begin(9600);
   // Inicialização dos pinos
+  pinMode(7, OUTPUT);
 }
 
 void loop() {
+  if((millis() - clockTrigger) > 100){
+    clockState = !clockState;
+    digitalWrite(7, clockState);
+    clockTrigger = millis();
+    Serial.println(clockState);
+  } 
   
 }
